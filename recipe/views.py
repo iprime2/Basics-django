4f86c7591cc0089042cac4fe6085775a6a584e36
Sushil Gupta <kalwarsushil2@gmail.com>
2023-08-30 22:04:22 +0530

    recipe

--- recipe/views.py
+++ recipe/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
 
 # Create your views here.
 
 
 def recipes(request):
-    return render(request, "recipes.html")
+    if request.method == "POST":
+        
+        data = request.POST
+        
+        recipe_image = request.FILES.get('recipe_img')
+        recipe_name = data.get('recipe_name')
+        recipe_description = data.get('recipe_desc')
+        
+        Recipe.objects.create(
+            recipe_name = recipe_name,
+            recipe_description = recipe_description,
+            recipe_image = recipe_image,
+        )
+        
+        return redirect('/recipes/')
+    
+    queryset = Recipe.objects.all()
+    context = {'recipes':queryset}
+    
+    return render(request, "recipes.html", context)
